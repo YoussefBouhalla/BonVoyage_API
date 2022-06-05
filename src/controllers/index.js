bc5464d54388ee47ba75259826549f8e6a59e871
UssefBhl <ybhl83@gmail.com>
2022-06-05 21:10:46 +0100

    🚧 created the controllers files

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+const UsersController = require("./Users");
+const StaysController = require("./Stays");
+const ReservationsController = require("./Reservations");
+const ToursController = require("./Tours");
+
+module.exports = {
+    UsersController,
+    StaysController,
+    ReservationsController,
+    ToursController
+}
