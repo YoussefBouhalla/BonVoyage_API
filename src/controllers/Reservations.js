041b74a0a1406ae5d8e3ba3f8623c76c03b52310
UssefBhl <ybhl83@gmail.com>
2022-06-06 11:19:05 +0100

    ✨ finished setting up controllers

--- src/controllers/Reservations.js
+++ src/controllers/Reservations.js
@@ -1,1 +1,25 @@
-module.exports = {}
+
+
+const getConnectedUserReservations = (req, res) => {}
+
+const deleteReservation = (req, res) => {}
+
+const acceptReservation = (req, res) => {}
+
+const refuseReservation = (req, res) => {}
+
+const getAllUserReservations = (req, res) => {}
+
+const getReservationsCount = (req, res) => {}
+
+const getUserStaysReservationsCount = (req, res) => {}
+
+module.exports = {
+    getConnectedUserReservations,
+    deleteReservation,
+    acceptReservation,
+    refuseReservation,
+    getAllUserReservations,
+    getReservationsCount,
+    getUserStaysReservationsCount
+}
